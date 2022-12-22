56203629fd90b21e487c441976284a84cf8d4503
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-12-22 18:34:32 +0100

    Registration via "Library"

--- Boolean.lua
+++ Boolean.lua
@@ -1,4 +1,9 @@
-Boolean = Boolean or {}
+local addOnName = ...
+
+--- @class Boolean
+local Boolean = {}
+
+Library.register(addOnName, '1.0.0', Boolean)
 
 function Boolean.toBoolean(value)
   return not not value
