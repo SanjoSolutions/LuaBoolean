9fd695887831f7af8fdf98032ddb8ed02b91c58a
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-12-23 17:47:19 +0100

    Reverts embedding approach

--- Boolean.lua
+++ Boolean.lua
@@ -1,13 +1,5 @@
-local addOnName = ...
-local version = '1.0.0'
+Boolean = {}
 
-if not Library.isRegistered(addOnName, version) then
-  --- @class Boolean
-  local Boolean = {}
-
-  Library.register(addOnName, version, Boolean)
-
-  function Boolean.toBoolean(value)
-    return not not value
-  end
+function Boolean.toBoolean(value)
+  return not not value
 end
