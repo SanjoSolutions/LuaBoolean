c98b28b9dd3f9f11b06f6f026ca87a20f6ac3d30
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-08-14 17:09:31 +0200

    Work on using Library in library packages

--- Boolean.lua
+++ Boolean.lua
@@ -1,17 +1,17 @@
 local addOnName = 'Boolean'
-local version = '1.0.0'
+local version = '2.0.0'
 
-if (_G.Library and not Library.isRegistered(addOnName, version)) or not _G.Library then
-  --- @class Boolean
-  local Boolean = {}
+if _G.Library then
+  if not Library.isRegistered(addOnName, version) then
+    --- @class Boolean
+    local Boolean = {}
 
-  function Boolean.toBoolean(value)
-    return not not value
-  end
+    function Boolean.toBoolean(value)
+      return not not value
+    end
 
-  if _G.Library then
     Library.register(addOnName, version, Boolean)
-  else
-    _G.Boolean = Boolean
   end
+else
+  error(addOnName + ' requires Library. It seems absent.')
 end
