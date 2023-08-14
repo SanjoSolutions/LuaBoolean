6484fac3eef31696435680aca989414269a82e5e
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-08-14 18:09:01 +0200

    Library add-ons with package.json

--- Boolean.lua
+++ Boolean.lua
@@ -13,5 +13,5 @@ if _G.Library then
     Library.register(addOnName, version, Boolean)
   end
 else
-  error(addOnName + ' requires Library. It seems absent.')
+  error(addOnName .. ' requires Library. It seems absent.')
 end
